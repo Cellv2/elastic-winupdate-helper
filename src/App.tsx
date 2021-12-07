5702a06d2609f48f39d65f6845953f2e72df3408
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2021-12-07 23:01:55 +0000

    Move reference components

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import RbCol from "react-bootstrap/Col";
 import RbContainer from "react-bootstrap/Container";
 import RbRow from "react-bootstrap/Row";
-import "./App.css";
+import styles from "./App.module.css";
 import BackupActions from "./components/BackupActions";
 import ClusterActions from "./components/ClusterActions";
 import ClusterInfo from "./components/ClusterInfo";
@@ -14,8 +14,8 @@ import logo from "./logo.svg";
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
+        <>
+            <div>
                 <RbContainer fluid>
                     <RbRow>
                         <RbCol xs={2} />
@@ -35,51 +35,55 @@ function App() {
                         </RbCol>
                     </RbRow>
                 </RbContainer>
-                <img src={logo} className="App-logo" alt="logo" />
-                <Counter />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <span>
-                    <span>Learn </span>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        React
-                    </a>
-                    <span>, </span>
-                    <a
-                        className="App-link"
-                        href="https://redux.js.org/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Redux
-                    </a>
-                    <span>, </span>
-                    <a
-                        className="App-link"
-                        href="https://redux-toolkit.js.org/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Redux Toolkit
-                    </a>
-                    ,<span> and </span>
-                    <a
-                        className="App-link"
-                        href="https://react-redux.js.org/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        React Redux
-                    </a>
-                </span>
-            </header>
-        </div>
+            </div>
+            <div className={styles.app}>
+                <header className={styles.appHeader}>
+                    <img src={logo} className={styles.appLogo} alt="logo" />
+                    <Counter />
+                    <p>
+                        Edit <code>src/App.tsx</code> and save to reload.
+                    </p>
+                    <span>
+                        <span>Learn </span>
+                        <a
+                            className={styles.appLink}
+                            href="https://reactjs.org/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            React
+                        </a>
+                        <span>, </span>
+                        <a
+                            className={styles.appLink}
+                            href="https://redux.js.org/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            Redux
+                        </a>
+                        <span>, </span>
+                        <a
+                            className={styles.appLink}
+                            href="https://redux-toolkit.js.org/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            Redux Toolkit
+                        </a>
+                        ,<span> and </span>
+                        <a
+                            className={styles.appLink}
+                            href="https://react-redux.js.org/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            React Redux
+                        </a>
+                    </span>
+                </header>
+            </div>
+        </>
     );
 }
 
