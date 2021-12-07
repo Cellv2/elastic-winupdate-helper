14fd372b76a2c1559a467d8ac5842c2da09072cc
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2021-12-07 23:32:54 +0000

    :lipstick: Separate items, add status/locked col

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,18 +1,40 @@
 import React from "react";
+import RbCol from "react-bootstrap/Col";
 import RbContainer from "react-bootstrap/Container";
+import RbRow from "react-bootstrap/Row";
 import Stack from "react-bootstrap/Stack";
+import styles from "./Sidebar.module.css";
 
 type Props = {};
 
 const Sidebar = (props: Props) => {
     return (
         <RbContainer fluid>
-            <div className="mb-2">Saved Clusters</div>
+            <div className={`${styles.centerText} mb-2 mx-0`}>
+                Configured Clusters
+            </div>
             <Stack gap={2}>
-                <input placeholder="Filter me" />
-                <div className="bg-light border">First item</div>
-                <div className="bg-light border">Second item</div>
-                <div className="bg-light border">Third item</div>
+                <RbRow>
+                    <input
+                        className={styles.centerText}
+                        placeholder="Filter Clusters"
+                    />
+                </RbRow>
+                <RbRow className="border">
+                    <RbCol xs={1}>G</RbCol>
+                    <RbCol xs={1}>L</RbCol>
+                    <RbCol className={styles.centerText}>Cluster Name</RbCol>
+                </RbRow>
+                <RbRow className="border">
+                    <RbCol xs={1}>G</RbCol>
+                    <RbCol xs={1}>L</RbCol>
+                    <RbCol className={styles.centerText}>Cluster Name</RbCol>
+                </RbRow>
+                <RbRow className="border">
+                    <RbCol xs={1}>G</RbCol>
+                    <RbCol xs={1}>L</RbCol>
+                    <RbCol className={styles.centerText}>Cluster Name</RbCol>
+                </RbRow>
             </Stack>
         </RbContainer>
     );
