0e3200ad8a390d569047885ac40afaa40ca06569
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2021-12-10 16:53:19 +0000

    Commit investigation on API responses

--- src/features/elastic/ElasticConnectButton.tsx
+++ src/features/elastic/ElasticConnectButton.tsx
@@ -4,11 +4,70 @@ import RbButton from "react-bootstrap/Button";
 
 type Props = {};
 
-const getClusterStats = async () => {
+type ElasticClusterHealthApiResponse = {
+    cluster_name: string;
+    status: string;
+    timed_out: boolean;
+    number_of_nodes: number;
+    number_of_data_nodes: number;
+    active_primary_shards: number;
+    active_shards: number;
+    relocating_shards: number;
+    initializing_shards: number;
+    unassigned_shards: number;
+    delayed_unassigned_shards: number;
+    number_of_pending_tasks: number;
+    number_of_in_flight_fetch: number;
+    task_max_waiting_in_queue_millis: number;
+    active_shards_percent_as_number: number;
+};
+
+type ElasticClusterStateMasterNodeApiResponse = {
+    cluster_name: string;
+    cluster_uuid: string;
+    master_node: string;
+};
+
+const getClusterStats = async (clusterUrl: string) => {
+    // we need to ensure that the url is correctly formatted for the fetches
+    let checkedClusterUrl = clusterUrl.trim();
+    if (!clusterUrl.startsWith("http://") && !clusterUrl.startsWith("https://")) {
+        console.warn("KEKW")
+        // TODO: Show notification saying we are setting this to http://
+        checkedClusterUrl = "http://" + clusterUrl;
+    }
+
+    if (clusterUrl.endsWith("/")) {
+        checkedClusterUrl = checkedClusterUrl.slice(0, -1)
+    }
+
     try {
-        const response = await fetch("http://localhost:9200/_nodes", {
+        // _cat/nodes?v=true&h=heap.percent,ram.percent,cpu,master,name,u
+        // do existing tools monitor host % cpu/ram or process
+        // name,jvm.mem.heap_used_percent,process.cpu.percent,process.mem.total_virtual_in_bytes,process.os.total_in_bytes
+
+        // https://www.elastic.co/guide/en/elasticsearch/reference/current/cluster-stats.html
+        // const response = await fetch("http://localhost:9200/_nodes/stats", {
+        const response = await fetch(checkedClusterUrl + "/_nodes/stats/os,process", {
             method: "GET",
         });
+
+        // https://www.elastic.co/guide/en/elasticsearch/reference/current/cluster-health.html
+        const clusterHealthResponse = await fetch( checkedClusterUrl + "/_cluster/health", {
+            method: "GET",
+        });
+        const clusterHealth: ElasticClusterHealthApiResponse =
+            await clusterHealthResponse.json();
+        console.log(clusterHealth)
+
+        // https://www.elastic.co/guide/en/elasticsearch/reference/current/cluster-state.html#cluster-state-api-path-params
+        const electedMasterResponse = await fetch(checkedClusterUrl + "/_cluster/state/master_node", {
+            method: "GET",
+        });
+        const electedMaster: ElasticClusterStateMasterNodeApiResponse =
+            await electedMasterResponse.json();
+        console.log(electedMaster);
+
         return await response.json();
     } catch (err) {
         console.error(err);
@@ -23,7 +82,7 @@ const ElasticConnectButton = (props: Props) => {
             event.preventDefault();
             // not sure this is needed yet
             // event.persist();
-            const clusterStats = await getClusterStats();
+            const clusterStats = await getClusterStats("http://localhost:9200");
             console.log(clusterStats);
         }
     };
@@ -44,7 +103,7 @@ const ElasticConnectButton = (props: Props) => {
             <RbForm>
                 <RbForm.Group className="mb-3" controlId="clusterName">
                     <RbForm.Label>Cluster Connection</RbForm.Label>
-                    <RbForm.Control type="email" placeholder="cluster:port" />
+                    <RbForm.Control type="text" placeholder="cluster:port" />
                     <RbForm.Text className="text-muted">
                         Please enter the URL of the cluster
                     </RbForm.Text>
